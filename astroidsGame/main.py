cbc6deec2be84e49eb13ff8cc6af4bc1eb1a8d45
Kubachoo <koichiseto61@gmail.com>
2024-09-30 12:22:12 -0400

    Added splitting astroids

--- astroidsGame/main.py
+++ astroidsGame/main.py
@@ -1,6 +1,9 @@
 import pygame
+import sys
 from constants import *
 from player import*
+from asteroid import*
+from asteroidfield import*
 
 def main():
     # initilizes pygame
@@ -12,16 +15,21 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
 
-    # These are all the objects that can be updated
+    # These are all the groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
-    # Adds Player to both groups
+    # Adds Player & Asteroid to thier respected groups
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(x, y)
-
-
+    asteroid = AsteroidField()
+    
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,6 +38,17 @@ def main():
         screen.fill("black")
         for object in updatable:
             object.update(dt)
+        
+        for asteroid in asteroids:
+            for bullet in shots:
+                if(asteroid.collisionCheck(bullet)):
+                    asteroid.split()
+                    bullet.kill()
+
+        for asteroid in asteroids:
+            if(asteroid.collisionCheck(player)):
+                print("Game Over!")
+                sys.exit()
 
         for object in drawable:
             object.draw(screen)
